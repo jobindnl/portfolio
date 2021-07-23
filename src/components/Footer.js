f2d409ac5a77b7363d221e42d2bcf55db059b613
jobindnl <jobindnl@gmail.com>
2021-07-23 02:44:43 -0400

    changed quotation marks on string src

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footer'>
-      <img src={process.env.PUBLIC_URL + "/javascript.png"} alt="javascript" />
-      <img src={process.env.PUBLIC_URL + "/html.png"} alt="html" />
-      <img src={process.env.PUBLIC_URL + "/css.png"} alt="css" />
-      <img src={process.env.PUBLIC_URL + "/mongo.png"} alt="mongo" />
-      <img src={process.env.PUBLIC_URL + "/express.png"} alt="express" />
-      <img src={process.env.PUBLIC_URL + "/react.png"} alt="react" />
-      <img src={process.env.PUBLIC_URL + "/node.png"} alt="node" />
+      <img src={process.env.PUBLIC_URL + '/javascript.png'} alt='javascript' />
+      <img src={process.env.PUBLIC_URL + '/html.png'} alt='html' />
+      <img src={process.env.PUBLIC_URL + '/css.png'} alt='css' />
+      <img src={process.env.PUBLIC_URL + '/mongo.png'} alt='mongo' />
+      <img src={process.env.PUBLIC_URL + '/express.png'} alt='express' />
+      <img src={process.env.PUBLIC_URL + '/react.png'} alt='react' />
+      <img src={process.env.PUBLIC_URL + '/node.png'} alt='node' />
     </div>
   )
 }
