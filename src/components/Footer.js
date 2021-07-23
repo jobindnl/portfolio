e02c2e9e56cb206ca3c72d82fc56a8814ed9a722
jobindnl <jobindnl@gmail.com>
2021-07-23 19:31:55 -0400

    completed responsive design on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,30 +8,16 @@ function Footer() {
       {/* beginning of editing responsive icons on footer */}
       <Container className='m-0'>
         <Row className='footer-icons pt-5 pb-5'>
-          <Col className='text-center p-md-0'>
+          <Col className='text-center p-0 p-md-0 d-inline-flex justify-content-around'>
             <img src={process.env.PUBLIC_URL + '/javascript.png'} alt='javascript' />
-          </Col>
-          <Col className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/html.png'} alt='html' />
-          </Col>
-          <Col className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/css.png'} alt='css' />
-          </Col>
-          <Col className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/bootstrap.png'} alt='bootstrap' />
           </Col>
-          {/* </Row>
-        <Row className='footer-icons pt-5 pb-5'> */}
-          <Col className='text-center p-xs-0'>
+          <Col className='text-center p-0 p-md-0 d-inline-flex justify-content-around'>
             <img src={process.env.PUBLIC_URL + '/mongo.png'} alt='mongo' />
-          </Col>
-          <Col  className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/express.png'} alt='express' />
-          </Col>
-          <Col  className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/react.png'} alt='react' />
-          </Col>
-          <Col  className='text-center p-xs-0'>
             <img src={process.env.PUBLIC_URL + '/node.png'} alt='node' />
           </Col>
         </Row>
