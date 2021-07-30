751e17dbf7af1072c5c9f066489f311b40b0478b
jobindnl <jobindnl@gmail.com>
2021-07-30 18:55:59 -0400

    changed logo width

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ function Navigation() {
       <Navbar expand='lg'>
         <Container>
           <Navbar.Brand>
-            <img width='80px' src={process.env.PUBLIC_URL + '/logo.png'} alt="site logo" />
+            <img width='60px' src={process.env.PUBLIC_URL + '/logo.png'} alt="site logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
